13cc1beabab7947ce0f347e0249dfedd83a24c60
Malte Jan Kaffenberger <malte@maltejk.de>
2020-05-10 17:47:34 +0200

    add cpio

--- install-deps.sh
+++ install-deps.sh
@@ -7,7 +7,7 @@ apt-get -y build-dep libreoffice
 apt-get -y install openssl g++ libssl-dev libpoco-dev libkrb5-dev libpng-dev \
 	libcap-dev libtool m4 automake libcppunit-dev libcppunit-doc pkg-config \
 	curl git devscripts fakeroot dh-systemd python-lxml python3-polib \
-	libpam-dev libreoffice libreoffice-dev libreofficekit-dev
+	libpam-dev cpio libreoffice libreoffice-dev libreofficekit-dev
 curl -sL https://deb.nodesource.com/setup_12.x | bash -
 apt-get install -y nodejs
 npm install -g jake
