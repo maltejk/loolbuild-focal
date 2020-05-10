d1a08887c7129608457b8af33b4e93b576b209c2
Malte Jan Kaffenberger <malte@maltejk.de>
2020-05-10 17:11:17 +0200

    add devscripts, fakeroot

--- install-deps.sh
+++ install-deps.sh
@@ -6,7 +6,7 @@ apt-get update
 apt-get -y build-dep libreoffice
 apt-get -y install openssl g++ libssl-dev libpoco-dev libkrb5-dev libpng-dev \
 	libcap-dev libtool m4 automake libcppunit-dev libcppunit-doc pkg-config \
-	curl git
+	curl git devscripts fakeroot
 curl -sL https://deb.nodesource.com/setup_12.x | bash -
 apt-get install -y nodejs
 npm install -g jake
